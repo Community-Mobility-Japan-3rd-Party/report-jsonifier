1f1599f2cb5e2f549f94f4658c3b0ef43544ddfd
MaySoMusician <maysomusician@gmail.com>
2020-06-28 15:16:08 +0900

    Address changes of CSV column formats

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,18 @@ function parsePromisified(
   });
 }
 
+/* Defined the only properties we just need */
+interface RawRecord {
+  sub_region_1: string;
+  date: string;
+  retail_and_recreation_percent_change_from_baseline: number;
+  grocery_and_pharmacy_percent_change_from_baseline: number;
+  parks_percent_change_from_baseline: number;
+  transit_stations_percent_change_from_baseline: number;
+  workplaces_percent_change_from_baseline: number;
+  residential_percent_change_from_baseline: number;
+}
+
 type DailyChangeRecord = {
   retailAndRecreation: number;
   groceryAndPharmacy: number;
@@ -29,15 +41,8 @@ type RecordsByDate = {
   [date: string]: DailyChangeRecord;
 };
 
-/* The following is an example of 'records':
- * [
- *  [ 'ALL', '2020-02-15', '-1', '4', '7', '1', '4', '0' ].
- *  [ 'ALL', '2020-02-16', '-9', '-6', '-35', '-10', '-2', '2' ],
- *  ...
- * ]
- */
 const writePrefectureRecords = (
-  records: string[][],
+  records: RawRecord[],
   prefecture: string,
   publishedOn: string,
   promises: Promise<void>[]
@@ -45,14 +50,13 @@ const writePrefectureRecords = (
   const recordsByDate: RecordsByDate = {};
 
   for (const r of records) {
-    // The value of r[1] woule be like '2020-02-15'
-    recordsByDate[r[1]] = {
-      retailAndRecreation: Number.parseInt(r[2]),
-      groceryAndPharmacy: Number.parseInt(r[3]),
-      parks: Number.parseInt(r[4]),
-      transitStations: Number.parseInt(r[5]),
-      workplaces: Number.parseInt(r[6]),
-      residential: Number.parseInt(r[7]),
+    recordsByDate[r.date] = {
+      retailAndRecreation: r.retail_and_recreation_percent_change_from_baseline,
+      groceryAndPharmacy: r.grocery_and_pharmacy_percent_change_from_baseline,
+      parks: r.parks_percent_change_from_baseline,
+      transitStations: r.transit_stations_percent_change_from_baseline,
+      workplaces: r.workplaces_percent_change_from_baseline,
+      residential: r.residential_percent_change_from_baseline,
     };
   }
 
@@ -74,13 +78,15 @@ const writePrefectureRecords = (
 };
 
 const processRecords = async (
-  records: string[][],
+  records: RawRecord[],
   publishedOn: string,
   promises: Promise<void>[]
 ): Promise<void> => {
-  const prefectures = new Set(records.map((r) => r[0]));
+  const prefectures = new Set(records.map((r) => r.sub_region_1));
   for (const p of prefectures) {
-    const recordsOfCurrentPrefecture = records.filter((r) => r[0] === p);
+    const recordsOfCurrentPrefecture = records.filter(
+      (r) => r.sub_region_1 === p
+    );
     writePrefectureRecords(
       recordsOfCurrentPrefecture,
       p,
@@ -102,15 +108,11 @@ const processRecords = async (
     console.log(`Reading ${f}`);
     const content = await fsp.readFile(`./origin/${f}`);
     const parsed = await parsePromisified(content, {
-      from_line: 2,
-      on_record: (record: string[]) => { // eslint-disable-line prettier/prettier
-        record.shift(); // Skip country_region_code
-        record.shift(); // Skip country_region
-        let area = record.shift(); // Get sub_region_1
-        if (area === '') area = 'ALL';
-        record.shift(); // Skip sub_region_2
-
-        return [area, ...record]; // e.g. [ 'ALL', '2020-02-15', '-1', '4', '7', '1', '4', '0' ]
+      from_line: 1,
+      columns: true,
+      on_record: (record: RawRecord) => { // eslint-disable-line prettier/prettier
+        if (record.sub_region_1 === '') record.sub_region_1 = 'ALL';
+        return record;
       },
     });
     processRecords(parsed, publishedOn, promisesWriting);
